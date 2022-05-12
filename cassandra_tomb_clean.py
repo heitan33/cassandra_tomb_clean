35c73968028f1aca1eac578303a75701534f5b3b
heitan33 <yyy3988@qq.com>
2022-05-12 15:55:22 +0800

    Add files via upload

--- cassandra_tomb_clean.py
+++ cassandra_tomb_clean.py
@@ -12,12 +12,16 @@ import argparse
 def cassandra_command_ops(cass_host_list, cassandra_command):
     for cass_host in cass_host_list:
         cass_host = cass_host.split(':')[0]
+        check_nodetool = "ls " + filename
         while True:
             os.environ['cassandra_command'] = str(cassandra_command)
-            status, remote_repair = commands.getstatusoutput("ssh root@%s $cassandra_command" %(cass_host))
+            os.environ['check_nodetool'] = str(check_nodetool)
+            status, remote_repair = commands.getstatusoutput("ssh root@%s $check_nodetool && $cassandra_command" %(cass_host))
             print(status, remote_repair)
             if status == 0:
                 break
+            else:
+                print(remote_repair)
 
 def cassandra_alter_gc(cass_host_list, graphindex_sql, edgestore_sql):
     for host in cass_host_list:
@@ -54,9 +58,9 @@ def main():
 
 if __name__ == "__main__":
     filename = '/opt/apache-cassandra-3.11.4/bin/nodetool'
-    if not os.path.exists(filename):
-        print("nodetool not exist!!")
-        sys.exit(2)
+#    if not os.path.exists(filename):
+#        print("nodetool not exist!!")
+#        sys.exit(2)
 
     parser = argparse.ArgumentParser(description='manual to this sceipt')
     parser.add_argument("--keyspace", type=str, required=True)
